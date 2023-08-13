8d26a039ca0ec0c4dd5d016c0a56e29193be31c6
anuragshuklajec <anuragshuklajec@gmail.com>
2023-08-13 18:56:23 +0530

    complete todo-list.js

--- week-1/Week-1-assignment-with-tests/01-js/hard/todo-list.js
+++ week-1/Week-1-assignment-with-tests/01-js/hard/todo-list.js
@@ -12,7 +12,48 @@
 */
 
 class Todo {
+  constructor(){
+    this.todoList = [] ;
+  }
+  add(todo){
+    this.todoList.push(todo)
+  }
+  remove(index){
+    this.todoList.splice(index,1)
+  }
+  update(index,updatedTodo){
+    this.todoList[index] = updatedTodo
+  }
+  getAll(){
+    console.log(this.todoList)
+  }
+  get(index){
+    console.log(this.todoList[index])
+  }
+  clear(){
+    this.todoList = []
+  }
+
+
 
 }
+
+todo = new Todo()
+todo.getAll()
+todo.add("Wake up at 10:00 AM")
+todo.getAll()
+todo.add("Brush Teeth")
+todo.getAll()
+todo.add("Go to Office at 11:00")
+todo.getAll()
+todo.remove(2)
+todo.getAll()
+todo.add("Bath")
+todo.getAll()
+todo.update(2,"Do yoga")
+todo.get(2)
+todo.getAll()
+todo.clear()
+
 
 module.exports = Todo;

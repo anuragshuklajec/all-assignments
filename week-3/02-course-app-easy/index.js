bc2a3b017b0554b7588269f9128c55d5d651c125
anuragshuklajec <anuragshuklajec@gmail.com>
2023-08-23 19:38:38 +0530

    complete course-selling-website-easy

--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -7,12 +7,24 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
-function validateUser(username,password){
+function validateAdmin(username,password){
   const adminIndex = ADMINS.findIndex(existingAdmin => existingAdmin.username == username); 
   if(adminIndex==-1){
     return false ;
   }else{
     if(ADMINS[adminIndex].password == password){
+      return true ;
+    }else{
+      return false ;
+    }
+  }
+}
+function validateUser(username,password){
+  const userIndex = USERS.findIndex(existingUser => existingUser.username == username); 
+  if(userIndex==-1){
+    return false ;
+  }else{
+    if(USERS[userIndex].password == password){
       return true ;
     }else{
       return false ;
@@ -38,7 +50,7 @@ app.post('/admin/login', (req, res) => {
   const adminUsername = req.headers.username
   const adminPassword = req.headers.password
 
-  if(validateUser(adminUsername,adminPassword)){
+  if(validateAdmin(adminUsername,adminPassword)){
     res.json({message:'Logged in succesfully'})
   }else{
     res.status(401).json("Invalid Credentials")
@@ -52,8 +64,8 @@ app.post('/admin/courses', (req, res) => {
   const adminPassword = req.headers.password
   let courseDetails = req.body
 
-  let auth = false ;
-  if(validateUser(adminUsername,adminPassword)){
+  
+  if(validateAdmin(adminUsername,adminPassword)){
     courseDetails["id"] = counter
     counter += 1 ;
     COURSES.push(courseDetails) ;
@@ -84,8 +96,8 @@ app.get('/admin/courses', (req, res) => {
   const adminUsername = req.headers.username
   const adminPassword = req.headers.password
   
-  let auth = false ;
-  if(validateUser(adminUsername,adminPassword)){
+  
+  if(validateAdmin(adminUsername,adminPassword)){
     res.json({courses : COURSES})
   }else{
     res.status(401).json("Invalid Credentials")
@@ -97,22 +109,69 @@ app.get('/admin/courses', (req, res) => {
 // User routes
 app.post('/users/signup', (req, res) => {
   // logic to sign up user
+  let user = req.body
+  user["purchasedCourses"] = []
+  const existingUser = USERS.find(event=>event.username == user.username)
+  if(existingUser){
+    res.status(403).json({message:"User already exists"}) ;
+  }else{
+    USERS.push(user) ; 
+    res.json({message:'User created succesfully'})
+  }
 });
 
 app.post('/users/login', (req, res) => {
   // logic to log in user
+  const userUsername = req.headers.username
+  const userPassword = req.headers.password
+
+  if(validateUser(userUsername,userPassword)){
+    res.json({message:'Logged in succesfully'})
+  }else{
+    res.status(401).json("Invalid Credentials")
+  }
 });
 
 app.get('/users/courses', (req, res) => {
   // logic to list all courses
+  const userUsername = req.headers.username
+  const userPassword = req.headers.password
+
+  
+  if(validateUser(userUsername,userPassword)){
+    res.json({courses : COURSES})
+  }else{
+    res.status(401).json("Invalid Credentials")
+  }
 });
 
 app.post('/users/courses/:courseId', (req, res) => {
   // logic to purchase a course
+  const userUsername = req.headers.username
+  const userPassword = req.headers.password
+  const courseId = req.params.courseId
+  console.log(courseId);
+  const courseIndex = COURSES.findIndex(course => course.id == courseId)
+  if(courseId != -1 && validateUser(userUsername,userPassword)){
+    const userIndex = USERS.findIndex(user => user.username == userUsername)
+    USERS[userIndex].purchasedCourses.push(COURSES[courseIndex])
+    console.log(COURSES[courseIndex]);
+    res.json({message : "Course purchased successfully "})
+  }else{
+    res.status(401).json({message : "Course can't be purchased"})
+  }
+
+
 });
 
 app.get('/users/purchasedCourses', (req, res) => {
-  // logic to view purchased courses
+  const userUsername = req.headers.username
+  const userPassword = req.headers.password
+  if (validateUser(userUsername,userPassword)){
+    const userIndex = USERS.findIndex(user => user.username == userUsername)
+    res.json({courses : USERS[userIndex].purchasedCourses})
+  }
+
 });
 
 app.listen(3000, () => {
